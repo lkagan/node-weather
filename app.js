304a82a224e6e0d5a20e720fccc6f7be166bf0d6
Larry Kagan <larry@superiocity.com>
2020-05-18 17:46:48 -0400

    Add error handling for geo service

--- app.js
+++ app.js
@@ -16,10 +16,16 @@ request({ url, json: true }, (error, response) => {
     }
 });
 
-// const locationUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGthZ2FuMjAwMCIsImEiOiJja2FjeWc4M2EwNDN5MnlsNzdvZm8ycWl3In0.YKxOq-XUqCahkO35Tni-cg&limit=1';
-//
-// request({ url: locationUrl, json: true }, (error, response) => {
-//     const [long, lat] = response.body.features[0].center;
-//     console.log(lat, long);
-// });
+const locationUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGthZ2FuMjAwMCIsImEiOiJja2FjeWc4M2EwNDN5MnlsNzdvZm8ycWl3In0.YKxOq-XUqCahkO35Tni-cg&limit=1';
+
+request({ url: locationUrl, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to geo service');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location, try with different search term.');
+    } else {
+        const [long, lat] = response.body.features[0].center;
+        console.log(lat, long);
+    }
+});
 
