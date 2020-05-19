c7f5436b48a1de7a6810650d124bc67f1e68729b
Larry Kagan <larry@superiocity.com>
2020-05-19 16:43:36 -0400

    Extract geocode into own module

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const request = require('postman-request');
+const geocode = require('./utils/geocode');
 
-const url = 'http://api.weatherstack.com/current?access_key=255fbaa3da49c33e8de8f41d931d9915&query=37.8267,-122.4233&units=f';
+// const url = 'http://api.weatherstack.com/current?access_key=255fbaa3da49c33e8de8f41d931d9915&query=37.8267,-122.4233&units=f';
 
-request({ url, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service');
-    } else if (response.body.error) {
-        console.log('Unable to find location');
-    } else {
-        const current = response.body.current;
-        const message = current.weather_descriptions[0] +
-            `. It is currently ${current.temperature} degrees out.  It feels ` +
-            `like ${current.feelslike} degrees out.`;
-        console.log(message);
-    }
+// request({ url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service');
+//     } else if (response.body.error) {
+//         console.log('Unable to find location');
+//     } else {
+//         const current = response.body.current;
+//         const message = current.weather_descriptions[0] +
+//             `. It is currently ${current.temperature} degrees out.  It feels ` +
+//             `like ${current.feelslike} degrees out.`;
+//         console.log(message);
+//     }
+// });
+
+// const locationUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGthZ2FuMjAwMCIsImEiOiJja2FjeWc4M2EwNDN5MnlsNzdvZm8ycWl3In0.YKxOq-XUqCahkO35Tni-cg&limit=1';
+//
+// request({ url: locationUrl, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to geo service');
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location, try with different search term.');
+//     } else {
+//         const [long, lat] = response.body.features[0].center;
+//         console.log(lat, long);
+//     }
+// });
+
+
+
+geocode('Palm Beach', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 });
-
-const locationUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGthZ2FuMjAwMCIsImEiOiJja2FjeWc4M2EwNDN5MnlsNzdvZm8ycWl3In0.YKxOq-XUqCahkO35Tni-cg&limit=1';
-
-request({ url: locationUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to geo service');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location, try with different search term.');
-    } else {
-        const [long, lat] = response.body.features[0].center;
-        console.log(lat, long);
-    }
-});
-
