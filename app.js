880cc9763d6f7dde1db6c2c9709cb6dc234b2418
Larry Kagan <larry@superiocity.com>
2020-05-19 17:04:42 -0400

    Extract forecast to own module

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const request = require('postman-request');
 const geocode = require('./utils/geocode');
-
-// const url = 'http://api.weatherstack.com/current?access_key=255fbaa3da49c33e8de8f41d931d9915&query=37.8267,-122.4233&units=f';
-
-// request({ url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service');
-//     } else if (response.body.error) {
-//         console.log('Unable to find location');
-//     } else {
-//         const current = response.body.current;
-//         const message = current.weather_descriptions[0] +
-//             `. It is currently ${current.temperature} degrees out.  It feels ` +
-//             `like ${current.feelslike} degrees out.`;
-//         console.log(message);
-//     }
-// });
-
-
+const forecast = require('./utils/forecast');
 
 geocode('Palm Beach', (error, data) => {
     console.log('Error', error);
     console.log('Data', data);
 });
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
