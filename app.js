9c0acadbafde2d09f353a053b58b0c70ff9ab45c
Larry Kagan <larry@superiocity.com>
2020-05-22 13:24:21 -0400

    Chain callbacks

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geocode('Palm Beach', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+    if (error) {
+        return console.log(error);
+    }
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data.location);
+        console.log(forecastData);
+    });
+});
