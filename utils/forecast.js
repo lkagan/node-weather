c47d0d3e0fee6089d129964920b11e2a1afb6e8e
Larry Kagan <larry@superiocity.com>
2020-05-22 14:14:22 -0400

    Refactor for property shorthand and object destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('postman-request');
 const forecast = (long, lat, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=255fbaa3da49c33e8de8f41d931d9915&query=' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '&units=f';
 
-    request({ url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service');
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location');
         } else {
-            const current = response.body.current;
+            const current = body.current;
             const message = current.weather_descriptions[0] +
                 `. It is currently ${current.temperature} degrees out.  It feels ` +
                 `like ${current.feelslike} degrees out.`;
